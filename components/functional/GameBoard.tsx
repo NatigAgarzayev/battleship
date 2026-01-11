fe09e8af8ed8535d19334276f100595e5a370e30
Natig <natigagharzayev@gmail.com>
2026-01-11 13:46:29 +0200

    feat: add dnd kit lib

--- components/functional/GameBoard.tsx
+++ components/functional/GameBoard.tsx
@@ -1,17 +1,20 @@
 import { IGameData } from '@/types/game'
 import React from 'react'
 import GameGrid from './GameGrid'
+import { DndContext } from '@dnd-kit/core'
 
 export default function GameBoard({ gameState }: { gameState: IGameData }) {
 
     return (
-        <div>
-            {gameState.player1_id &&
-                <GameGrid playerId={gameState.player1_id} playerName={gameState.player1_name} />
-            }
-            {gameState.player2_id &&
-                <GameGrid playerId={gameState.player2_id} playerName={gameState.player2_name} />
-            }
-        </div>
+        <DndContext>
+            <div>
+                {gameState.player1_id &&
+                    <GameGrid playerId={gameState.player1_id} playerName={gameState.player1_name} />
+                }
+                {gameState.player2_id &&
+                    <GameGrid playerId={gameState.player2_id} playerName={gameState.player2_name} />
+                }
+            </div>
+        </DndContext>
     )
 }
